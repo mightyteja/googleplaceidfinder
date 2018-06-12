f37d26af1001db4ff6d58f05b18afdecf18857b4
mightyteja <jaisreram2003@gmail.com>
2018-06-12 19:55:22 +0530

    Moving the file to live server

--- js/scketch.js
+++ js/scketch.js
@@ -4,11 +4,9 @@ var proxy = "https://cors-anywhere.herokuapp.com/"; // To bypass the Cross Domai
 var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=';
 var query = $("#query"); //'Best Hotels in chennai';// // Getting the input from User
 var key = '&key=';
-// xhr.setRequestHeader('Content-Type', 'application/json');
 var keyInput = $('#key');  // Key for authorization
 var placeDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
 var output = '';
-// var placeID = 'ChIJN1t_tDeuEmsRUsoyG83frY4';
 function loadText() {
     // Create XHR request
     var xhr = new XMLHttpRequest();//OPEN - type url/file , async
@@ -16,25 +14,20 @@ function loadText() {
     // xhr.open('GET', proxy + url + query + key + keyInput, true); 
     xhr.onload = function xhr(result) {
         if (this.readyState == 4 && this.status == 200) {
-            if (result.length !== 0) {
-                var user = JSON.parse(this.responseText);
-                var placeID = user.results;
-                // output += placeID[0].place_id;
-                // console.log(output);
-                for (var i = 0; i < placeID.length; i++) {
-                    output += placeID[i].name + ' - ' + placeID[i].place_id + '<br/>' + '<hr/>';
-                }
-                // console.log(output);
-                document.getElementById('output_place').innerHTML = output;
-                //code for the page details starts                  
-                //https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=YOUR_API_KEY
-            }//end of onload if statement
-            else {
-                console.log("No Data");
-
+            var user = JSON.parse(this.responseText);
+            var placeID = user.results;
+            // output += placeID[0].place_id;
+            // console.log(output);
+            for (var i = 0; i < placeID.length; i++) {
+                output += placeID[i].name + ' - ' + placeID[i].place_id + '<br/>' + '<hr/>';
             }
-        }
-
+            status += user.status;
+            // console.log(output);
+            document.getElementById('output_place').innerHTML = output;
+            document.getElementById('error_output').innerHTML = status;
+            //code for the page details starts                  
+            //https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=YOUR_API_KEY
+        }//end of onload if statement
     } // end of onload
     xhr.send();
 }
